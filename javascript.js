75be3ff50662fe3f35f5e41ad3800c754a0e06d4
iUnknownGalaxy <18318229+iUnknownGalaxy@users.noreply.github.com>
2024-01-11 15:45:11 -0500

    feat: Create 16x16 grid using divs

--- javascript.js
+++ javascript.js
@@ -1,7 +1,14 @@
 const container = document.getElementById("container");
-for(let i = 1; i <= 256; i++) {
+for(let i = 1; i <= 16; i++) {
     const div = document.createElement("div");
+    div.style = "width: 10px; height: 10px";
     container.appendChild(div);
+    for(let i = 1; i <= 16; i++) {
+        const divChild = document.createElement("div");
+        divChild.style = "width: 10px; height: 10px";
+        div.appendChild(divChild);
+    }
+    
 }
 
 container.style = "display: flex";

804e21fd8d6049ef35b473bc6b9396f848c83518
iUnknownGalaxy <18318229+iUnknownGalaxy@users.noreply.github.com>
2024-01-11 23:21:11 -0500

    feat: Add prompt to let user change the grid size

--- javascript.js
+++ javascript.js
@@ -1,11 +1,14 @@
 const container = document.getElementById("container");
-for(let i = 1; i <= 256; i++) {
+
+const CONTAINER_WIDTH = 800;
+boxAmount = prompt("How many boxes?");
+boxSize = CONTAINER_WIDTH / boxAmount;
+for(let i = 1; i <= boxAmount * boxAmount; i++) {
     const div = document.createElement("div");
-    div.style = "height: 50px; width: 50px;"
+    div.style = `height: ${boxSize}px; width: ${boxSize}px;`
     container.appendChild(div);
     div.addEventListener('mouseover', () => {
         div.style.backgroundColor = "blue";
     })
 }
-
-container.style = "display: flex; flex-wrap: wrap;";
+container.style = "display: flex; flex-wrap: wrap;border-style: solid; border-width: 5px; border-color: red;";
